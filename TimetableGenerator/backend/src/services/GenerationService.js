a3c18922be5362cb97f66fd8cfcd3ce3f61bf0dd
Biró Botond <birbotond@yahoo.com>
2025-04-19 14:14:36 +0300

    Using grpc instead of write and read from file in backend

--- TimetableGenerator/backend/src/services/GenerationService.js
+++ TimetableGenerator/backend/src/services/GenerationService.js
@@ -1,41 +1,52 @@
-import { exec } from 'child_process'
-import { writeFileSync } from 'fs';
 import classService from './ClassService.js'
 import locationService from './LocationService.js'
 import teacherService from './TeacherService.js'
 import { getTimetableData } from './ImportExportService.js';
 
-function OsFunction () {
-  this.execCommand = function (cmd) {
-    return new Promise((resolve, reject) => {
-      exec(cmd, (error, stdout) => {
-        if (error) {
-          reject(error);
-        }
-        resolve(stdout);
-      });
-    });
-  };
-}
+var PROTO_PATH = './../../generator.proto';
 
-const osFunc = new OsFunction();
+import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var generatorProto = grpc.loadPackageDefinition(packageDefinition).generator;
+
+var target = 'localhost:50051';
 
 export const generate = async () => {
   try {
-    const inputString = await getTimetableData();
-    await writeFileSync('../../TimetableGeneratorEngine/TimetableGenerator/in.json', inputString);
-    await osFunc.execCommand('TimetableGenerator.exe').then(async result => {
-      const catalogs = JSON.parse(result);
-      console.log(`Active: ${  catalogs.active}`);
-      console.log(`Class fitnes: ${  catalogs.fitnesClas}`);
-      console.log(`Teacher fitnes: ${  catalogs.fitnesTeacher}`);
-      console.log(`Location fitnes: ${  catalogs.fitnesLocation}`);
-      console.log(`Elapsed time: ${  catalogs.elapsedTime}`);
+    var client = new generatorProto.Generator(target,
+                                          grpc.credentials.createInsecure());
 
-      await updateCatalogs(catalogs);
-    }).catch(err => {
-      console.log('os >>>', err);
+    const inputString = await getTimetableData();
+
+    console.log('start generation')
+
+    const response = await new Promise((resolve, reject) => {
+      client.Generate({ input: inputString }, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
     });
+
+    const catalogs = JSON.parse(response.output);
+    console.log(`Active: ${catalogs.active}`);
+    console.log(`Class fitnes: ${catalogs.fitnesClas}`);
+    console.log(`Teacher fitnes: ${catalogs.fitnesTeacher}`);
+    console.log(`Location fitnes: ${catalogs.fitnesLocation}`);
+    console.log(`Elapsed time: ${catalogs.elapsedTime}`);
+
+    await updateCatalogs(catalogs);
+
   } catch (e) {
     console.log(e);
   }
